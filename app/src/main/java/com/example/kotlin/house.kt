f8a8b2aa6679f668aa7b22d00be8a7e4fb500ff6
asam89 <asammanis8900@gmail.com>
2024-03-05 14:34:01 +0700

    halo

---
+++ app/src/main/java/com/example/kotlin/house.kt
@@ -0,0 +1,28 @@
+package com.example.kotlin
+
+class House {
+    val houseColor:String = "white"
+    val numberOfWindows: Int = 2
+    val isForSale: Boolean = false
+    fun updateColor O{
+
+    }
+
+    fun putOnSale){
+
+        }
+
+    }
+
+fun main() {
+
+    val A1 House()
+    val A2 = House()
+    val A3 = House()
+
+    println(A1.houseColor)
+    println("=".repeat(20))
+    println(A2)
+    println("=".repeat(20))
+    println(A3)
+}
