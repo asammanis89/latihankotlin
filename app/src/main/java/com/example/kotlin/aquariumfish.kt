a0bcbea358c4b97612a31c7ddd1b58c691519cdb
asam89 <asammanis8900@gmail.com>
2024-03-19 08:23:57 +0700

    baru

--- app/src/main/java/com/example/kotlin/aquariumfish.kt
+++ app/src/main/java/com/example/kotlin/aquariumfish.kt
@@ -1,2 +1,21 @@
 package com.example.kotlin
+abstract class AquariumFish {
+    abstract val color: String
 
+}
+class Shark: AquariumFish(), FishAction {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus: AquariumFish(), FishAction {
+    override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+interface FishAction  {
+    fun eat()
+}
--- app/src/main/java/com/example/kotlin/compactfunction.kt
+++ app/src/main/java/com/example/kotlin/compactfunction.kt
@@ -1,2 +1,6 @@
 package com.example.kotlin
 
+fun main () {
+    val a = {x:Int -> x*2}
+    println (a(2))
+}
